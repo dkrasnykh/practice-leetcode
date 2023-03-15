fede24d24d2efdd14530bf2da7bfc9b86b678e5e
Darya Krasnykh <krasnykh.daria@gmail.com>
2023-03-15 10:22:27 +0300

    array easy

---
+++ src/main/java/array/replace_elements_with_greatest_element_on_right_side_1299/Solution.java
@@ -0,0 +1,30 @@
+package array.replace_elements_with_greatest_element_on_right_side_1299;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Solution {
+    public int[] replaceElements(int[] arr) {
+        Map<Integer, Integer> map = new TreeMap<>((a, b) -> b - a);
+        for (Integer e : arr) {
+            map.compute(e, (key, value) -> (value == null) ? 1 : value + 1);
+        }
+        for (int i = 0; i < arr.length - 1; ++i) {
+            if (map.get(arr[i]) == 1) {
+                map.remove(arr[i]);
+            } else {
+                map.compute(arr[i], (key, value) -> value - 1);
+            }
+            arr[i] = map.entrySet().iterator().next().getKey();
+        }
+        arr[arr.length - 1] = -1;
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(Arrays.toString(s.replaceElements(new int[]{17, 18, 5, 4, 6, 1})));
+        System.out.println(Arrays.toString(s.replaceElements(new int[]{400})));
+    }
+}
---
+++ src/main/java/matrix/word_search_ii/Solution.java
@@ -0,0 +1,13 @@
+package matrix.word_search_ii;
+
+import java.util.List;
+
+class Solution {
+    public List<String> findWords(char[][] board, String[] words) {
+        return null;
+    }
+    public static void main(String[] args) {
+        char[][] board={{'o','a','a','n'},{'e','t','a','e'},{'i','h','k','r'},{'i','f','l','v'}};
+        String[] words = {"oath","pea","eat","rain"};
+    }
+}
