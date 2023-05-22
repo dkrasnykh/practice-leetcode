9653b12fad71e9d0c19bf923aa664645932d77b1
Darya Krasnykh <krasnykh.daria@gmail.com>
2023-05-22 19:27:02 +0300

    84 206

---
+++ src/main/java/linked_list/reverse_linked_list_206/iterative/ListNode.java
@@ -0,0 +1,52 @@
+package linked_list.reverse_linked_list_206.iterative;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode current = head.next;
+        ListNode previous = head;
+        previous.next = null;
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        return previous;
+    }
+
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        ListNode res = s.reverseList(node1);
+        int test = 5;
+    }
+}
